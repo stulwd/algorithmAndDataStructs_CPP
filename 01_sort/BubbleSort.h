78f0ec1af5945c4cede1489b29a5c96fad1ba534
刘文迪 <stulwd@163.com>
2020-10-23 18:00:44 +0800

    Create BubbleSort.h

---
+++ 01_sort/BubbleSort.h
@@ -0,0 +1,20 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+
+#include <iostream>
+#include "Comparable.h"
+
+class BubbleSort
+{
+private:
+    /* data */
+    static bool greater(Comparable*, Comparable*);
+    static void exch(Comparable* [], int, int);
+public:
+    BubbleSort(/* args */);
+    ~BubbleSort();
+
+    static void sort(Comparable* [], int);
+};
+
+#endif
